684cb5c4334b2b1466f6c44a47180239e932080e
EuSoJao <89408008+EuSoJao@users.noreply.github.com>
2024-08-21 15:54:12 -0300

    site upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+let h1;
+
+function setup() {
+  createCanvas(600, 400);
+  h1 = createElement('h1', `${keyCode}`)
+  h1.position(300, 200)
+  h1.style('text-align', 'center')
+}
+
+function draw() {
+  background(220);
+  keyDetect()
+  //keyText()
+}
+
+function keyDetect(){
+    if(keyIsPressed === true){
+    console.log(`${keyCode} ${key}`)
+    h1.html(`${keyCode}`)
+    }
+}
+
+function keyText(){
+  createElement('h1', keyCode)
+}
